d59a2a3ae76769ab028877c692dca0ffac175b1f
Antoine Gersant <antoine.gersant@lesforges.org>
2016-09-21 23:32:30 -0700

    Formatting

--- src/ui/headless.rs
+++ src/ui/headless.rs
@@ -2,8 +2,8 @@ use std::time;
 use std::thread;
 
 pub fn run() {
-	println!("Starting up UI (headless)");
-	loop {
-		thread::sleep(time::Duration::from_secs(10));
-	}
+    println!("Starting up UI (headless)");
+    loop {
+        thread::sleep(time::Duration::from_secs(10));
+    }
 }
