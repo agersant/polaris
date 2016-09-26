b6c741d0e5bafe6a6237c1626e543d12c67fcece
Antoine Gersant <antoine.gersant@lesforges.org>
2016-09-25 17:18:11 -0700

    Tidied up resource directory structure

--- build_release_windows.ps1
+++ build_release_windows.ps1
@@ -1,9 +1,9 @@
 "Compiling resource file"
-RC /fo res\application.res res\application.rc
+RC /fo res\windows\application\application.res res\windows\application\application.rc
 
 ""
 "Compiling executable"
-cargo rustc --release --features "ui" -- -C link-args="/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup res\application.res"
+cargo rustc --release --features "ui" -- -C link-args="/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup res\windows\application\application.res"
 
 ""
 "Creating output directory"
@@ -12,18 +12,18 @@ Remove-Item -Recurse .\release\tmp\*
 
 ""
 "Copying to output directory"
-Copy-Item .\target\release\polaris.exe 	.\release\tmp\
-Copy-Item .\res\libeay32.dll 			.\release\tmp\
-Copy-Item .\res\libeay32md.dll 			.\release\tmp\
-Copy-Item .\res\default_config.toml 	.\release\tmp\polaris.toml
-Copy-Item .\web\ 						.\release\tmp\ -recurse
-Copy-Item .\res\license.rtf				.\release\tmp\
-Copy-Item .\res\banner.bmp				.\release\tmp\
-Copy-Item .\res\dialog.bmp				.\release\tmp\
+Copy-Item .\res\windows\libeay32.dll 			.\release\tmp\
+Copy-Item .\res\windows\libeay32md.dll 			.\release\tmp\
+Copy-Item .\res\windows\installer\license.rtf	.\release\tmp\
+Copy-Item .\res\windows\installer\banner.bmp	.\release\tmp\
+Copy-Item .\res\windows\installer\dialog.bmp	.\release\tmp\
+Copy-Item .\target\release\polaris.exe 			.\release\tmp\
+Copy-Item .\res\default_config.toml 			.\release\tmp\polaris.toml
+Copy-Item .\web\ 								.\release\tmp\ -recurse
 
 ""
 "Creating installer"
-candle -wx -arch x64 					-out .\release\tmp\installer.wixobj 	.\res\installer.wxs
+candle -wx -arch x64 					-out .\release\tmp\installer.wixobj 	.\res\windows\installer\installer.wxs
 light  -wx -spdb -ext WixUIExtension	-out .\release\Polaris_0.1.0.msi 		.\release\tmp\installer.wixobj
 
 "Cleaning up"
