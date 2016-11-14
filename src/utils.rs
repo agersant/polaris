d9abb35e4dc69683f9d379e7b489fb8a8913f876
Antoine Gersant <antoine.gersant@lesforges.org>
2016-11-13 16:44:31 -0800

    Extracted audio format detection from is_song()

--- src/utils.rs
+++ src/utils.rs
@@ -1,21 +1,34 @@
 use std::path::Path;
 
-pub fn is_song(path: &Path) -> bool {
+pub enum AudioFormat {
+	FLAC,
+	MP3,
+	MP4,
+	MPC,
+	OGG,
+}
+
+pub fn get_audio_format(path: &Path) -> Option<AudioFormat> {
 	let extension = match path.extension() {
 		Some(e) => e,
-		_ => return false,
+		_ => return None,
 	};
 	let extension = match extension.to_str() {
 		Some(e) => e,
-		_ => return false,
+		_ => return None,
 	};
 	match extension.to_lowercase().as_str() {
-		"mp3" => true,
-		"ogg" => true,
-		"m4a" => true,
-		"flac" => true,
-		_ => false,
+		"flac" => Some(AudioFormat::FLAC),
+		"mp3" => Some(AudioFormat::MP3),
+		"m4a" => Some(AudioFormat::MP4),
+		"mpc" => Some(AudioFormat::MPC),
+		"ogg" => Some(AudioFormat::OGG),
+		_ => None,
 	}
+}
+
+pub fn is_song(path: &Path) -> bool {
+	get_audio_format(path).is_some()
 }
 
 pub fn is_image(path: &Path) -> bool {
