baadd410ca9534f5f318204aa5810e46c251d8e6
Antoine Gersant <antoine.gersant@lesforges.org>
2017-05-29 19:13:03 -0700

    Added packaging script and makefile for Linux install

---
+++ build_release_unix.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "Creating output directory"
+mkdir -p release/tmp
+
+echo "Copying package files"
+cp -r web src Cargo.toml Cargo.lock res/unix/Makefile release/tmp
+cp res/default_config.toml release/tmp/polaris.toml
+
+echo "Creating tarball"
+POLARIS_VERSION=$(grep ^version Cargo.toml | awk '{print $3}' | tr -d '"\r\n')
+tar -zc -C release/tmp -f release/polaris-$POLARIS_VERSION.tar.gz .
+
+echo "Cleaning up"
+rm -rf release/tmp
