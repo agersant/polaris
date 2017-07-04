559c4de0dd05d89775dec91c1f2bd27cc2322433
Antoine Gersant <antoine.gersant@lesforges.org>
2017-07-04 04:01:33 -0700

    Simplify test

--- test/nightwatch/first_time_flow.js
+++ test/nightwatch/first_time_flow.js
@@ -2,8 +2,7 @@ module.exports = {
   'Welcome page loads' : function (browser) {
     browser
       .url('http://localhost:5050')
-      .waitForElementVisible('#initial-setup-page', 1000);
-	browser.expect.element("#initial-setup-page h2").text.to.contain("Welcome to Polaris");
+      .waitForElementVisible('body', 1000);
     browser.end();
   }
 };
