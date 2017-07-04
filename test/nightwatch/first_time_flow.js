007a0c7a41e576ca8f36cfffee2c72ee86a0086c
Antoine Gersant <antoine.gersant@lesforges.org>
2017-07-04 01:05:02 -0700

    Fixed test for first time flow load

--- test/nightwatch/first_time_flow.js
+++ test/nightwatch/first_time_flow.js
@@ -2,8 +2,8 @@ module.exports = {
   'Welcome page loads' : function (browser) {
     browser
       .url('http://localhost:5050')
-      .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'Welcome to Polaris')
-      .end();
+      .waitForElementVisible('#initial-setup-page', 1000);
+	browser.expect.element("#initial-setup-page h2").text.to.contain("Welcome to Polaris");
+    browser.end();
   }
 };
