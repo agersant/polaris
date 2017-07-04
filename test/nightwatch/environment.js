2091286953813b6c1d5884d81caa9e181fe10277
Antoine Gersant <antoine.gersant@lesforges.org>
2017-07-04 02:50:06 -0700

    Better output about why polyfill is missing

--- test/nightwatch/environment.js
+++ test/nightwatch/environment.js
@@ -1,7 +1,7 @@
-var assert = require('assert');
+var assert = require("assert");
 
 module.exports = {
-  'Fetch Web API' : function(browser) {
-    assert(typeof fetch == "function", "fetch function is not available");
-  }
+	'Fetch Web API' : function(browser) {
+		assert.equal(typeof fetch, "function", "fetch function is not available");
+	}
 };
