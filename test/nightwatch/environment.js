7cdfc73f0e1ab7c8c0c54e1f00d03ea9477ef30d
Antoine Gersant <antoine.gersant@lesforges.org>
2017-07-04 02:16:01 -0700

    Cosmetic change

--- test/nightwatch/environment.js
+++ test/nightwatch/environment.js
@@ -2,6 +2,6 @@ var assert = require('assert');
 
 module.exports = {
   'Fetch Web API' : function(browser) {
-    assert(typeof fetch == "function");
+    assert(typeof fetch == "function", "fetch function is not available");
   }
 };
