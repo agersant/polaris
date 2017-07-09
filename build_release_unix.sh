2351d62b67917670f28fc552da64b01e1d41ce59
Antoine Gersant <antoine.gersant@lesforges.org>
2017-07-08 17:54:37 -0700

    Fixed a bug where linux builds had bad names

--- build_release_unix.sh
+++ build_release_unix.sh
@@ -7,7 +7,7 @@ cp -r web src Cargo.toml Cargo.lock res/unix/Makefile release/tmp
 cp res/default_config.toml release/tmp/polaris.toml
 
 echo "Creating tarball"
-POLARIS_VERSION=$(grep ^version Cargo.toml | awk '{print $3}' | tr -d '"\r\n')
+POLARIS_VERSION=$(grep -m 1 ^version Cargo.toml | awk '{print $3}' | tr -d '"\r\n')
 tar -zc -C release/tmp -f release/polaris-$POLARIS_VERSION.tar.gz .
 
 echo "Cleaning up"
