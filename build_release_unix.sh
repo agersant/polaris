f2e6a27c0e9d6b0d4eb9534ceb99b693fda5af57
Antoine Gersant <antoine.gersant@lesforges.org>
2019-09-27 22:42:20 -0700

    Wrap content of unix releases in a top-level folder

--- build_release_unix.sh
+++ build_release_unix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 echo "Creating output directory"
-mkdir -p release/tmp
+mkdir -p release/tmp/polaris
 
 echo "Copying package files"
-cp -r web docs/swagger src migrations Cargo.toml Cargo.lock res/unix/Makefile release/tmp
+cp -r web docs/swagger src migrations Cargo.toml Cargo.lock res/unix/Makefile release/tmp/polaris
 
 echo "Creating tarball"
 POLARIS_VERSION=$(grep -m 1 ^version Cargo.toml | awk '{print $3}' | tr -d '"\r\n')
-tar -zc -C release/tmp -f release/polaris-$POLARIS_VERSION.tar.gz .
+tar -zc -C release/tmp -f release/polaris-$POLARIS_VERSION.tar.gz polaris
 
 echo "Cleaning up"
 rm -rf release/tmp
