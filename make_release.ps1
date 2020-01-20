bb85b4793287094165536edc3f458c7ec891f49f
Antoine Gersant <antoine.gersant@lesforges.org>
2020-01-20 14:25:21 -0800

    Get latest polaris web before making a release

--- make_release.ps1
+++ make_release.ps1
@@ -4,5 +4,6 @@ Get-ChildItem "Cargo.toml" | ForEach-Object {
 	$script:POLARIS_VERSION = $matches[1]
 }
 
+git submodule update --recursive --remote
 git tag -a $POLARIS_VERSION -m "Polaris $POLARIS_VERSION"
 git push origin $POLARIS_VERSION
