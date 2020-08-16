1218b14bfa52549b48b3b1ba2031fc1b35f0da29
Antoine Gersant <antoine.gersant@lesforges.org>
2020-08-16 15:28:46 -0700

    On Linux, read locations from environment variables at compile time

--- src/directories/linux.rs
+++ src/directories/linux.rs
@@ -7,7 +7,7 @@ pub struct Directories {}
 
 impl Directories {
 	pub fn get_pid_directory() -> Result<PathBuf> {
-		let path = Path::new("/run/polaris");
+		let path = Path::new(option_env!("POLARIS_PID_DIR").unwrap_or("."));
 		fs::create_dir_all(&path)?;
 		Ok(path.to_owned())
 	}
@@ -15,25 +15,26 @@ impl Directories {
 
 impl PolarisDirectories for Directories {
 	fn get_log_directory() -> Result<PathBuf> {
-		let path = Path::new("/var/log/polaris");
+		let path = Path::new(option_env!("POLARIS_LOG_DIR").unwrap_or("."));
 		fs::create_dir_all(&path)?;
 		Ok(path.to_owned())
 	}
 
 	fn get_db_directory() -> Result<PathBuf> {
-		let path = Path::new("/var/lib/polaris");
+		let path = Path::new(option_env!("POLARIS_DB_DIR").unwrap_or("."));
 		fs::create_dir_all(&path)?;
 		Ok(path.to_owned())
 	}
 
 	fn get_thumbnail_directory() -> Result<PathBuf> {
-		let path = Path::new("/var/cache/polaris/thumbnails");
+		let mut path = Path::new(option_env!("POLARIS_CACHE_DIR").unwrap_or(".")).to_owned();
+		path.push("thumbnails");
 		fs::create_dir_all(&path)?;
 		Ok(path.to_owned())
 	}
 
 	fn get_static_directory() -> Result<PathBuf> {
-		let path = Path::new("/usr/share/polaris");
+		let path = Path::new(option_env!("POLARIS_STATIC_DIR").unwrap_or("."));
 		fs::create_dir_all(&path)?;
 		Ok(path.to_owned())
 	}
