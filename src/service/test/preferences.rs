f976e64ea531dc0d6d1ce0cce3ce7439e6a9ee42
Antoine Gersant <antoine.gersant@lesforges.org>
2020-11-29 17:03:39 -0800

    Test auth in preference tests

--- src/service/test/preferences.rs
+++ src/service/test/preferences.rs
@@ -4,7 +4,15 @@ use crate::config;
 use crate::service::test::{constants::*, ServiceType, TestService};
 
 #[test]
-fn test_service_preferences() {
+fn test_get_preferences_requires_auth() {
+	let mut service = ServiceType::new(&format!("{}{}", TEST_DB_PREFIX, line!()));
+	let request = service.request_builder().get_preferences();
+	let response = service.fetch(&request);
+	assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[test]
+fn test_get_preferences_golden_path() {
 	let mut service = ServiceType::new(&format!("{}{}", TEST_DB_PREFIX, line!()));
 	service.complete_initial_setup();
 	service.login();
@@ -12,6 +20,23 @@ fn test_service_preferences() {
 	let request = service.request_builder().get_preferences();
 	let response = service.fetch_json::<_, config::Preferences>(&request);
 	assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[test]
+fn test_put_preferences_requires_auth() {
+	let mut service = ServiceType::new(&format!("{}{}", TEST_DB_PREFIX, line!()));
+	let request = service
+		.request_builder()
+		.put_preferences(config::Preferences::default());
+	let response = service.fetch(&request);
+	assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[test]
+fn test_put_preferences_golden_path() {
+	let mut service = ServiceType::new(&format!("{}{}", TEST_DB_PREFIX, line!()));
+	service.complete_initial_setup();
+	service.login();
 
 	let request = service
 		.request_builder()
