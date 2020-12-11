71c797ec7c6691150a64222a1f346b06f0df324c
Antoine Gersant <antoine.gersant@lesforges.org>
2020-12-10 22:02:26 -0800

    Removed unused manage state

--- src/service/actix/mod.rs
+++ src/service/actix/mod.rs
@@ -16,8 +16,7 @@ pub mod test;
 pub fn make_config(context: service::Context) -> impl FnOnce(&mut ServiceConfig) + Clone {
 	move |cfg: &mut ServiceConfig| {
 		let encryption_key = cookie::Key::derive_from(&context.auth_secret[..]);
-		cfg.app_data(web::Data::new(context.db))
-			.app_data(web::Data::new(context.index))
+		cfg.app_data(web::Data::new(context.index))
 			.app_data(web::Data::new(context.config_manager))
 			.app_data(web::Data::new(context.lastfm_manager))
 			.app_data(web::Data::new(context.playlist_manager))
