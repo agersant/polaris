bd48ad1a5c9fc8343046f2034b9bbf56bd1bbc70
Antoine Gersant <antoine.gersant@lesforges.org>
2020-12-15 01:32:58 -0800

    Adds jpeg to default album art filter

--- migrations/201706272304_misc_settings_table/up.sql
+++ migrations/201706272304_misc_settings_table/up.sql
@@ -4,4 +4,4 @@ CREATE TABLE misc_settings (
 	index_sleep_duration_seconds INTEGER NOT NULL,
 	index_album_art_pattern TEXT NOT NULL
 );
-INSERT INTO misc_settings (id, auth_secret, index_sleep_duration_seconds, index_album_art_pattern) VALUES (0, hex(randomblob(64)), 1800, "Folder.(jpg|png)");
+INSERT INTO misc_settings (id, auth_secret, index_sleep_duration_seconds, index_album_art_pattern) VALUES (0, hex(randomblob(64)), 1800, "Folder.(jpeg|jpg|png)");
